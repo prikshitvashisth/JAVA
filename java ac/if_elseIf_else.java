c49a7959e0eb9235ee8ad4f39fe3ef4909290a34
Prikshit Vashisth <prikshit1304vashisth@gmail.com>
2025-09-26 01:30:47 +0530

    auto-commit

--- java ac/if_elseIf_else.java
+++ java ac/if_elseIf_else.java
@@ -10,7 +10,7 @@ public class if_elseIf_else {
         } else if (a == 2) {
             System.out.println("tuesday");
         } else if (a == 3) {
-            System.out.println("wednesda");
+            System.out.println("wednesday");
         } else if (a == 4) {
             System.out.println("thursday");
         } else if (a == 5) {

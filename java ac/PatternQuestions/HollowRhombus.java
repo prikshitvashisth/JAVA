b2d12fbcd0a4dadf3a48b5bcb9201bbc6614d4b4
Prikshit Vashisth <prikshit1304vashisth@gmail.com>
2026-01-09 22:16:37 +0530

    HollowRhombus

--- java ac/PatternQuestions/HollowRhombus.java
+++ java ac/PatternQuestions/HollowRhombus.java
@@ -1,27 +1,28 @@
 package PatternQuestions;
 import java.util.Scanner;
 
-public class Rhombus {
+public class HollowRhombus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+        System.out.print("Enter size of hollow rhombus: ");
+        int n = sc.nextInt();   // size
 
         for (int i = 1; i <= n; i++) {
 
-
-            // spaces
+            // Print leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print("  ");
             }
 
-            // stars
+            // Print hollow square of stars
             for (int j = 1; j <= n; j++) {
-                System.out.print("* ");
+                if (i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
             }
 
             System.out.println();
         }
     }
 }
-

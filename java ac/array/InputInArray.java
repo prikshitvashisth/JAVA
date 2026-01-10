0aab69d6f9dfd1480f1ce927e4be5e1b4c03ce3d
Prikshit Vashisth <prikshit1304vashisth@gmail.com>
2026-01-10 22:50:03 +0530

    Takeing inpute in java

---
+++ java ac/array/InputInArray.java
@@ -0,0 +1,18 @@
+package array;
+
+public class InputInArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        
+        // Simulating input into the array
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 10; // Example input: multiples of 10
+        }
+        
+        // Displaying the contents of the array
+        System.out.println("Array contents:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }    
+}

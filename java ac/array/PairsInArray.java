6c0d014cecbab8746492c34fd74833bff7f32ad2
Prikshit Vashisth <prikshit1304vashisth@gmail.com>
2026-02-17 23:49:07 +0530

    Pairs in an Array

---
+++ java ac/array/PairsInArray.java
@@ -0,0 +1,19 @@
+package array;
+
+public class PairsInArray {
+	// Prints all pairs in the array
+	public static void printPairs(int numbers[]) {
+		for (int i = 0; i < numbers.length; i++) {
+			int curr = numbers[i];
+			for (int j = i + 1; j < numbers.length; j++) {
+				System.out.print("(" + curr + "," + numbers[j] + ") ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String args[]) {
+		int numbers[] = {2, 4, 6, 8, 10};
+		printPairs(numbers);
+	}
+}

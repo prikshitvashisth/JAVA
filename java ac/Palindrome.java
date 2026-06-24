30135e67fbed20913c3b682e35025a8e604142e7
Prikshit Vashisth <prikshit1304vashisth@gmail.com>
2026-06-25 00:01:33 +0530

    Palindrome

--- java ac/Palindrome.java
+++ java ac/Palindrome.java
@@ -1,29 +1,36 @@
 import java.util.Scanner;
 
-public class Palindrome {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string to check if it's a palindrome: ");
-        String str = scanner.nextLine();
-        boolean isPalindrome = isPalindrome(str);
-        if (isPalindrome) {
-            System.out.println(str + " is a palindrome.");
-        } else {
-            System.out.println(str + " is not a palindrome.");
-        }
-    }
-
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
+class Palindrome {
+    
+    public static int[] finalDestination(String direction){
+        int x = 0;  
+        int y = 0;
+        
+        for(int i = 0; i < direction.length(); i++){
+            if (direction.charAt(i) == 'N'){
+                y++;
+            } else if(direction.charAt(i) == 'S'){
+                y--;
+            } else if(direction.charAt(i) == 'E'){
+                x++;
+            } else if(direction.charAt(i) == 'W'){
+                x--;
             }
-            left++;
-            right--;
         }
-        return true;
+        int[] arr = {x,y};
+        return arr;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the direction string (N, S, E, W): ");
+        String direction = sc.nextLine();
+        direction = direction.toUpperCase(); 
+        int[] arr = finalDestination(direction);
+        int x = arr[0];
+        int y = arr[1];
+        double desplacement = Math.hypot(x,y);
+        
+        System.out.print("The displacement between (0,0) and (" + x + "," + y + ") is: " + desplacement);
     }
 }

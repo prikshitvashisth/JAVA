5bd19c6ac20309dc1826462557555fb3458d9279
Prikshit Vashisth <136953371+prikshitvashisth@users.noreply.github.com>
2026-06-27 22:17:48 +0530

    Update tax calculation threshold from 500k to 450k

--- java ac/TaxCalculater.java
+++ java ac/TaxCalculater.java
@@ -11,11 +11,11 @@ public class TaxCalculater {
         float salary = income;
         
 
-        if (income <= 500000) {
+        if (income <= 450000) {
             System.out.println("Tax are 0 your total salary is: " +income);
             
         }
-        else if (income > 500000 && income <= 1000000){
+        else if (income > 450000 && income <= 1000000){
             tax = income*0.2f;
             salary = income - tax;
             System.out.println("Total tax on your salary is 20% so your taxex are: "+tax +  "your salary after tax is: "+salary);

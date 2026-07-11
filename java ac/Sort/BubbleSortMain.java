6c2fa009f643d9235896aa8a157d9fbb32bfea93
Prikshit Vashisth <136953371+prikshitvashisth@users.noreply.github.com>
2026-07-11 22:03:44 +0530

    Fix formatting issues in BubbleSortMain.java

--- java ac/Sort/BubbleSortMain.java
+++ java ac/Sort/BubbleSortMain.java
@@ -3,7 +3,7 @@ package Sort;
 class BubbleSort {
     public void bubblesortFxn(int[] arr) {
         int n = arr.length;
-
+ 
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {

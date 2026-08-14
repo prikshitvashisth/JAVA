c30dd811d40902e12bed3531cecdde857d0ae1ac
Prikshit Vashisth <prikshit1304vashisth@gmail.com>
2026-08-14 23:18:25 +0530

    updated mearge sort

--- java ac/MergeSort.java
+++ java ac/MergeSort.java
@@ -45,4 +45,12 @@ public class MergeSort {
             k++;
         }
     }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        mergeSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }

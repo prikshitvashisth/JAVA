cf57561b0ef4d1121958bbf4c8eaea2630d8621c
Prikshit Vashisth <prikshit1304vashisth@gmail.com>
2026-08-14 23:16:39 +0530

    mearge sort

---
+++ java ac/MergeSort.java
@@ -0,0 +1,48 @@
+public class MergeSort {
+    public static void mergeSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+        int[] temp = new int[arr.length];
+        mergeSortHelper(arr, temp, 0, arr.length - 1);
+    }
+
+    private static void mergeSortHelper(int[] arr, int[] temp, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSortHelper(arr, temp, left, mid);
+        mergeSortHelper(arr, temp, mid + 1, right);
+        merge(arr, temp, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        for (int i = left; i <= right; i++) {
+            temp[i] = arr[i];
+        }
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        while (i <= mid && j <= right) {
+            if (temp[i] <= temp[j]) {
+                arr[k] = temp[i];
+                i++;
+            } else {
+                arr[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid) {
+            arr[k] = temp[i];
+            i++;
+            k++;
+        }
+        while (j <= right) {
+            arr[k] = temp[j];
+            j++;
+            k++;
+        }
+    }
+}
